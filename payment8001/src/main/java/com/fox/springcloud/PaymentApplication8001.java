454fe2c13d4ecbee2b4ce8478ff39bee6a835012
liuyunzhen <1124421329@qq.com>
2021-05-20 22:05:59 +0800

    引入eureka注册中心

--- payment8001/src/main/java/com/fox/springcloud/PaymentApplication8001.java
+++ payment8001/src/main/java/com/fox/springcloud/PaymentApplication8001.java
@@ -2,8 +2,10 @@ package com.fox.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentApplication8001 {
 
     public static void main(String[] args) {
