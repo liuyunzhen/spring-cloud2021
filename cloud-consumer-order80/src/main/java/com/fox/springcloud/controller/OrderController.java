95f67be2c037c4b3339841a670392153635153b6
liuyunzhen <1124421329@qq.com>
2021-05-30 21:13:50 +0800

    restTemplate getForEntity

--- cloud-consumer-order80/src/main/java/com/fox/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/fox/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.fox.springcloud.controller;
 import com.fox.springcloud.entity.CommonResult;
 import com.fox.springcloud.entity.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -26,5 +27,15 @@ public class OrderController {
     @RequestMapping("/custmer/payment/get")
     public CommonResult<Payment> get(Long id){
         return restTemplate.getForObject(preffix_url + "/payment/get?id=" + id, CommonResult.class);
+    }
+
+    @RequestMapping("/custmer/payment/getEntity")
+    public CommonResult<Payment> getEntity(Long id){
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(preffix_url + "/payment/get?id=" + id, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        } else {
+            return new CommonResult<>(500, "操作失败");
+        }
     }
 }
