d14c7ef46420b67e9e10c0ad5a6f1a405015b550
liuyunzhen <1124421329@qq.com>
2021-06-01 06:33:25 +0800

    修改默认负载规则，自定义配置使用随机负载

--- cloud-consumer-order80/src/main/java/com/fox/springcloud/OrderApplication80.java
+++ cloud-consumer-order80/src/main/java/com/fox/springcloud/OrderApplication80.java
@@ -3,9 +3,12 @@ package com.fox.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import rule.MySelfRule;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name="cloud-provider-payment", configuration = MySelfRule.class)
 public class OrderApplication80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication80.class, args);
