9c8873eb4986373d2191b93e587ab94fe60866a9
liuyunzhen <1124421329@qq.com>
2021-06-05 20:23:44 +0800

    DefaultProperties

--- cloud-hystrix-openfeign-order80/src/main/java/com/fox/springcloud/controller/OrderController.java
+++ cloud-hystrix-openfeign-order80/src/main/java/com/fox/springcloud/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.fox.springcloud.controller;
 
 import com.fox.springcloud.service.OrderService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
@@ -12,22 +13,25 @@ import javax.annotation.Resource;
 
 @RestController
 @Slf4j
+@DefaultProperties(defaultFallback = "global_fallback")
 public class OrderController {
 
     @Resource
     private OrderService orderService;
 
-    @HystrixCommand(fallbackMethod = "paymentInfo_timeout_fallback", commandProperties = {
-            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value = "15000")
-    })
+//    @HystrixCommand(fallbackMethod = "paymentInfo_timeout_fallback", commandProperties = {
+//            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value = "4000")
+//    })
+    @HystrixCommand
     @GetMapping("/consumer/payment/hystrix/ok/{id}")
     public String paymentInfo(@PathVariable("id") Long id){
         return orderService.paymentInfo(id);
     }
 
-    @HystrixCommand(fallbackMethod = "paymentInfo_timeout_fallback", commandProperties = {
-            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value = "9000")
-    })
+//    @HystrixCommand(fallbackMethod = "paymentInfo_timeout_fallback", commandProperties = {
+//            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value = "9000")
+//    })
+    @HystrixCommand
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
     public String paymentInfo_timeout(@PathVariable("id") Long id){
         return orderService.paymentInfo_timeout(id);
@@ -35,5 +39,9 @@ public class OrderController {
 
     public String paymentInfo_timeout_fallback(@PathVariable("id") Long id){
         return "我是订单模块80，支付模块业务繁忙请稍后再试" + id;
+    }
+
+    public String global_fallback(){
+        return "全局异常服务降级处理";
     }
 }
