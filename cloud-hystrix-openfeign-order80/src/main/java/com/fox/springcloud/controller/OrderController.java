a3d5c639bab0396cf99a0fdbece4327564a955cc
liuyunzhen <1124421329@qq.com>
2021-06-05 17:03:43 +0800

    服务降级(服务消费者)

--- cloud-hystrix-openfeign-order80/src/main/java/com/fox/springcloud/controller/OrderController.java
+++ cloud-hystrix-openfeign-order80/src/main/java/com/fox/springcloud/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.fox.springcloud.controller;
 
 import com.fox.springcloud.service.OrderService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,13 +17,23 @@ public class OrderController {
     @Resource
     private OrderService orderService;
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_timeout_fallback", commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value = "15000")
+    })
     @GetMapping("/consumer/payment/hystrix/ok/{id}")
     public String paymentInfo(@PathVariable("id") Long id){
         return orderService.paymentInfo(id);
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_timeout_fallback", commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value = "9000")
+    })
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
     public String paymentInfo_timeout(@PathVariable("id") Long id){
         return orderService.paymentInfo_timeout(id);
+    }
+
+    public String paymentInfo_timeout_fallback(@PathVariable("id") Long id){
+        return "我是订单模块80，支付模块业务繁忙请稍后再试" + id;
     }
 }
