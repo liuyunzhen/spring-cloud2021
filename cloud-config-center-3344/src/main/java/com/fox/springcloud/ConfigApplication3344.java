0198f97b1bce53fd4d3e62ff8d73590a3f464399
liuyunzhen <1124421329@qq.com>
2021-06-12 17:47:52 +0800

    config分布式配置中心模块搭建

---
+++ cloud-config-center-3344/src/main/java/com/fox/springcloud/ConfigApplication3344.java
@@ -0,0 +1,13 @@
+package com.fox.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication3344.class, args);
+    }
+}
