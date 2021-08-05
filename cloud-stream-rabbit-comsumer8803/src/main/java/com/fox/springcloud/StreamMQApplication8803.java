9dacdb73c0ee737bf4d63036ccaab687dec4603c
liuyunzhen <1124421329@qq.com>
2021-08-05 21:25:24 +0800

    解决重复消费和持久化问题

---
+++ cloud-stream-rabbit-comsumer8803/src/main/java/com/fox/springcloud/StreamMQApplication8803.java
@@ -0,0 +1,11 @@
+package com.fox.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamMQApplication8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQApplication8803.class, args);
+    }
+}
