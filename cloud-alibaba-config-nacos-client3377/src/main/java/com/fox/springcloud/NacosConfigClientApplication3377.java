7394c89d0a1823ea3d3693714b81b2f068a0ef9a
liuyunzhen <1124421329@qq.com>
2021-08-11 07:03:49 +0800

    nacos注册中心

---
+++ cloud-alibaba-config-nacos-client3377/src/main/java/com/fox/springcloud/NacosConfigClientApplication3377.java
@@ -0,0 +1,13 @@
+package com.fox.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientApplication3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientApplication3377.class, args);
+    }
+}
