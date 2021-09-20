b57d163c45a8a19ed445559b7ef6c59c0382584e
liuyunzhen <1124421329@qq.com>
2021-09-20 22:42:28 +0800

    @SentinelResource注解属性

--- cloud-alibaba-consumer-nacos-order84/src/main/java/com/fox/springcloud/controller/CircleBreakerController.java
+++ cloud-alibaba-consumer-nacos-order84/src/main/java/com/fox/springcloud/controller/CircleBreakerController.java
@@ -21,7 +21,7 @@ public class CircleBreakerController {
     private RestTemplate restTemplate;
 
     @GetMapping("/consume/echo/{id}")
-    @SentinelResource(value = "fallback", fallback = "handler_fallback")
+    @SentinelResource(value = "fallback", fallback = "handler_fallback", blockHandler = "handler_block", exceptionsToIgnore = {IllegalArgumentException.class})
     public CommonResult<String> callback(@PathVariable Long id){
         if (id == 4){
             throw new IllegalArgumentException("IllegalArgumentException 非法参数异常。。。");
@@ -30,6 +30,10 @@ public class CircleBreakerController {
     }
 
     public CommonResult<String> handler_fallback(@PathVariable Long id, Throwable e){
-        return new CommonResult<>(500, "全局兜底异常处理：" + e.getMessage());
+        return new CommonResult<>(500, "全局兜底业务异常处理：" + e.getMessage());
+    }
+
+    public CommonResult<String> handler_block(@PathVariable Long id, BlockException e){
+        return new CommonResult<>(543, "全局兜底流控规则异常处理：" + e.getClass().getName());
     }
 }
